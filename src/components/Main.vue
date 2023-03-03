11dbe8dc70c499dbc7b6dd3b7fadc41837401ad3
TheDusty117 <gabrieledipassio@gmail.com>
2023-03-03 16:22:27 +0100

    FEAT try to fetch cards from api

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,4 +1,7 @@
 <script>
+//1___importo axios per effettuare le chiamate all'api
+import axios from 'axios'
+
 export default {
   data() {
     return {
@@ -8,6 +11,14 @@ export default {
   methods: {
     fetchCards() {
       console.log('fetching data')
+      //qui viene fatta chiamata in get a endopoint:
+      axios
+        .get('https://db.ygoprodeck.com/api/v7/cardinfo.php')
+        //uso arrow function per poter avere un this dove accedere alle informazioni interne
+        .then((res)=> {
+          console.log(res.data.data)
+          this.cards = res.data.data
+        })
     }
   },
   created(){
@@ -20,9 +31,13 @@ export default {
 <template>
   <main>
     <div class="container">
-      <div class="row">
-
-      </div>
+      <ul>
+        <li v-for="card in cards" :key="card.id" >
+          <img :src="card.card_images.image_url_cropped" alt="">
+          <h2>{{ card.name }}</h2>
+          <h3></h3>
+        </li>
+      </ul>
     </div>
   </main>
 </template>
