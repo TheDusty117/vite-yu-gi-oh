1628630ea1d24c71e027c18e0b094b925c0294f9
TheDusty117 <gabrieledipassio@gmail.com>
2023-03-03 15:11:23 +0100

    FEAT main structure

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-4">YUGIOH</div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import bootstrap from 'bootstrap'
+
+  export default{
+    components: {
+      bootstrap
+    }
+
+
+  }
+
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
